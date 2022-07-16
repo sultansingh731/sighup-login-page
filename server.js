586d3d70fbbee4ca6293040549c44db9b58e127f
sultan singh <sultansingh731@gmail.com>
2022-07-16 22:54:33 +0530

    configured signup and login :+1:

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-const express = require(express);
+const express = require("express");
+
 const app = express();
 require("dotenv").config();
-app.use(express.json())
-app.use(express.static())
+app.use(express.json());
 
-const port = process.env.port || 2000;
+app.post("/api/login", loginHandler);
+app.post("/api/signup", signupHandler);
 
-app.post("api/login", loginHandler);
-app.post("api/signup", signupHandler);
+// find email in database if exist throw error
+//  save all credential and reponnse ok
 
 function signupHandler(req, res) {
-    const { name, email, password } = req.body;
+  const { name, email, password, cpassword } = req.body;
+  res.json(req.body);
 }
-function loginHandler(req, res) {
-    const {email, password } = req.body;
 
+// find email if not exit throw error || user not found
+// if email exist then match it with password else invalid password
+//if match login successfully
+
+function loginHandler(req, res) {
+  const { email, password } = req.body;
+  res.json(req.body);
 }
+const port = process.env.port || 2000;
 
 app.listen(port, () => {
   console.log(`server is listeing on ${port}`);
